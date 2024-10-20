ef4d8db33d565eb3bb166191cd9edf83355b3f2f
RozeJa <92266408+RozeJa@users.noreply.github.com>
2024-10-20 19:28:28 +0200

    spousta prace xd

--- js/info.js
+++ js/info.js
@@ -1,21 +1,20 @@
-const headers = Array.from(document.querySelectorAll("h2"))
-headers.unshift(document.querySelector("h1"))
+const bookmarks = Array.from(document.querySelectorAll(".info-container-bookmark"))
 
 setInterval(() => {
     const links = Array.from(document.querySelector(".info-nav").querySelectorAll("li"))
 
-    let lastHeader = -1
-    headers.forEach((e,i) => {
-        if (e.getBoundingClientRect().top < 100) {
-            lastHeader = i
+    let lastBookmark = -1
+    bookmarks.forEach((e,i) => {
+        if (e.getBoundingClientRect().top < 10) {
+            lastBookmark = i
         }
     })
     
-    if (lastHeader === -1) {
+    if (lastBookmark === -1) {
         links[0].classList.add("info-nav-active")
         links.splice(1).forEach(e => e.classList.remove("info-nav-active"))
     } else {        
-        let selectedLink = links[lastHeader]
+        let selectedLink = links[lastBookmark]
 
         links.forEach(e => e.classList.remove("info-nav-active"))
         selectedLink.classList.add("info-nav-active")
